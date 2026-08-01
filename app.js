2188aa942fefadaec5ebf848df1b592e8ffac8ca
ciromellado <ciromellado@gmail.com>
2026-08-01 11:21:28 -0400

    Update Supabase client import to version 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CHATVEN-LIGHT: LÓGICA DE INTERFAZ Y DATOS
 // ==========================================
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL = 'https://cwfhufcvnmvwpppfpcuy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3Zmh1ZmN2bm12d3BwcGZwY3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1Nzc1MDUsImV4cCI6MjEwMTE1MzUwNX0.rSCUty5TPlBz8mIo6x5xp-xigpxraCt6kfLAjwowP0I';
